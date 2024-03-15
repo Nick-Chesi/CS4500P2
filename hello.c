889e718850e0981ca5a205a64df3501040dae8a2
Nick-Chesi <143734499+Nick-Chesi@users.noreply.github.com>
2024-03-15 16:07:26 -0600

    Update hello.c

--- hello.c
+++ hello.c
@@ -2,16 +2,19 @@
 #include <linux/kernel.h>
 // Header files ^
 
-int init_module(void){
+// Renamed from init_module, was getting compiler issues
+int helloModule(void){
         printk(KERN_INFO "HELLO WORLD! \n");
         return 0;
 }
 
-void cleanup_module(void){
+// Same as before, renamed from cleanup_module
+void cleanupModule(void){
         printk(KERN_INFO "GOODBYE WORLD! \n");
 }
 
-module_init(init_module);
-module_exit(cleanup_module);
+// Added the corrected function names down here
+module_init(helloModule);
+module_exit(cleanupModule);
 
 MODULE_LICENSE("GPL");
